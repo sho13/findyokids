5b1df6211cce573d2201b4e3b64b0dd81fb5ffe5
Sean Ho <seansinflipflops@gmail.com>
2017-05-14 03:24:10 -0400

    conflicts

--- client/src/containers/app.jsx
+++ client/src/containers/app.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Slider from 'react-slick';
+import axios from 'axios';
+
+
 
 class App extends Component {
   constructor(props) {
