7df29c36d595b2617fb8c2002f1d693c04577e1e
Sean Ho <seansinflipflops@gmail.com>
2017-05-14 03:13:28 -0400

    shit

--- client/src/containers/app.jsx
+++ client/src/containers/app.jsx
@@ -1,36 +1,61 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Slider from 'react-slick';
 
 class App extends Component {
-
-  sideImages() {
-    for(let i = 562; i < 633; i++) {
-    console.log('./images/side/IMG_5' + i + '.jpg');
-      return (
-        <div>
-          <img src={require('./images/side/IMG_5' + i + '.jpg')} alt='#'/>
-        </div>
-      )
+  constructor(props) {
+    super(props);
+    this.state = {
+      front: null,
+      side: null
     }
   }
 
+  componentDidMount() {
+    axios.get('/images')
+      .then((data) => {
+        this.setState( 
+          {
+          front: data.data.front,
+          side: data.data.side
+        })
+      })
+  }
+
+  images() {
+    if(this.state.front === null && this.state.side === null) {
+      return (
+        <div>Loading</div>
+      )
+    }
+    return this.state.front.map((val) => {
+      axios.post('/clarifai', {front:val} )
+           .then((response) => {
+             console.log("RESSSSPONSE:", response);
+           })
+      return (
+        <div>
+          <img width='400' height='400' src={'/front/' + val} /> 
+        </div>
+      )
+    });
+  }
   
   render() {  
     const settings = {
-    	dots: true,
       infinite: true,
       autoplay: true,
       autoplaySpeed: 5000,
       speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      swipeToSlide: true,
+      slidesToShow: 1000,
+      slidesToScroll: 1000,
     }
 
+    
     return (
       <div>
         <Slider {...settings}>
-          <div style>{this.sideImages()}</div>
+          {this.images()}
         </Slider>
       </div>
     )
--- server/server.js
+++ server/server.js
@@ -2,12 +2,35 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const testFolder = './images/side';
+const testFolder2 = './images/front';
+const fs = require('fs');
+
+
 
 app.use(express.static(path.join(__dirname, '/../client/')));
+app.use(express.static(path.join(__dirname, '/../images/')));
 app.use(express.static(path.join(__dirname, '/../node_modules')));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
+
+const side = fs.readdirSync(testFolder).map(file => {
+  return file;
+})
+
+const front = fs.readdirSync(testFolder2).map(file => {
+  return file;
+})
+
+const image = {
+  side: side,
+  front: front,
+}
+
+app.get('/images', (req, res) => {
+  res.send(image);
+})
 
 app.listen(port, (err) => {
   if(err) {
